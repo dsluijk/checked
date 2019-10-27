8a229f2c4bdf8ff40d494fcf2914f1f4baf2cc6f
Dany Sluijk <dany@atlasdev.nl>
2019-10-27 13:59:51 +0100

    chore: clean up the ssh handling

--- src/ssh.c
+++ src/ssh.c
@@ -1,107 +1,51 @@
 #include "ssh.h"
 
-static void chan_close(ssh_session session, ssh_channel channel, void *userdata) {
-    int fd = *(int*)userdata;
-    (void)session;
-    (void)channel;
-
-    close(fd);
+static void sshClose(ssh_session session, ssh_channel channel, void *userdata) {
+  printf("Connection closed\n");
 }
 
-static int copy_chan_to_fd(ssh_session session,
-                                           ssh_channel channel,
-                                           void *data,
-                                           uint32_t len,
-                                           int is_stderr,
-                                           void *userdata) {
-    int fd = *(int*)userdata;
-    int sz;
-    (void)session;
-    (void)channel;
-    (void)is_stderr;
+static int sshRead(ssh_session sess, ssh_channel chan, void *data, uint32_t len,
+                   int is_stderr, void *userdata) {
+  char *p = data;
+  char key = *(p + len - 1);
+  if (key == 67) {
+    char out[5] = "Hello";
+    ssh_channel_write(chan, out, 5);
+  }
 
-    char buf2[2048];
-    memcpy(buf2, data, len);
-    printf("%#02x\n", buf2[len-1]);
-    if(buf2[len-1] == 67) {
-      printf("Hello\n");
-    }
-    // sz = write(fd, data, len);
-    return sz;
+  return 1;
 }
 
-static int copy_fd_to_chan(socket_t fd, int revents, void *userdata) {
-    ssh_channel chan = (ssh_channel)userdata;
-    char buf[2048];
-    int sz = 0;
-
-    if(!chan) {
-        close(fd);
-        return -1;
-    }
-
-    if(revents & POLLIN) {
-        sz = read(fd, buf, 2048);
-        if(sz > 0) {
-            ssh_channel_write(chan, buf, sz);
-        }
-    }
-
-    if(revents & POLLHUP) {
-        ssh_channel_close(chan);
-        sz = -1;
-    }
-
-    return sz;
-}
-
-void startSSH() {
-  ssh_session session;
-  ssh_bind sshbind;
+void handleConnection(ssh_session session) {
   ssh_message message;
   ssh_channel chan = 0;
   char buf[2048];
-  int auth = 0;
-  int sftp = 0;
-  int i;
-  int r;
-
-  sshbind = ssh_bind_new();
-  session = ssh_new();
-
-  int port = 4242;
-  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDPORT, &port);
-  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_HOSTKEY, "ssh-rsa");
-  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_RSAKEY, KEYS_FILE);
-
-  if (ssh_bind_listen(sshbind) < 0) {
-    printf("Error listening to socket: %s\n", ssh_get_error(sshbind));
-    return 1;
-  }
-
-  r = ssh_bind_accept(sshbind, session);
-  if (r == SSH_ERROR) {
-    printf("error accepting a connection : %s\n", ssh_get_error(sshbind));
-    return 1;
-  }
+  char *user;
 
   if (ssh_handle_key_exchange(session)) {
     printf("ssh_handle_key_exchange: %s\n", ssh_get_error(session));
-    return 1;
+    return;
   }
 
+  bool done = false;
   do {
     message = ssh_message_get(session);
-    if (!message)
-      break;
+
+    if (!message) {
+      printf("auth error: %s\n", ssh_get_error(session));
+      ssh_disconnect(session);
+      return;
+    }
 
     switch (ssh_message_type(message)) {
     case SSH_REQUEST_AUTH:
       switch (ssh_message_subtype(message)) {
       case SSH_AUTH_METHOD_NONE:
-        printf("Logging in as %s\n", ssh_message_auth_user(message));
-        auth = 1;
+        user = malloc(strlen(ssh_message_auth_user(message)) + 1);
+        memcpy(user, ssh_message_auth_user(message),
+               strlen(ssh_message_auth_user(message)));
         ssh_message_auth_reply_success(message, 0);
+        done = true;
       default:
         ssh_message_auth_set_methods(message, SSH_AUTH_METHOD_NONE);
         ssh_message_reply_default(message);
@@ -113,13 +57,7 @@ void startSSH() {
     }
 
     ssh_message_free(message);
-  } while (!auth);
-
-  if (!auth) {
-    printf("auth error: %s\n", ssh_get_error(session));
-    ssh_disconnect(session);
-    return 1;
-  }
+  } while (!done);
 
   do {
     message = ssh_message_get(session);
@@ -136,91 +74,72 @@ void startSSH() {
       ssh_message_free(message);
     }
   } while (message && !chan);
+
   if (!chan) {
     printf("error : %s\n", ssh_get_error(session));
     ssh_finalize();
-    return 1;
+    return;
   }
 
-  do {
-    message = ssh_message_get(session);
-    if (message && ssh_message_type(message) == SSH_REQUEST_CHANNEL && ssh_message_subtype(message) == SSH_CHANNEL_REQUEST_PTY) {
-      ssh_message_channel_request_reply_success(message);
-      break;
-    }
-
-    ssh_message_reply_default(message);
-    
-    
-    ssh_message_free(message);
-  } while (message && !sftp);
-
-  printf("it works!\n");
-
-
-
-
   struct ssh_channel_callbacks_struct cb = {
-	.channel_data_function = copy_chan_to_fd,
-    .channel_eof_function = chan_close,
-    .channel_close_function = chan_close,
-	.userdata = NULL 
-};
+      .channel_data_function = sshRead,
+      .channel_eof_function = sshClose,
+      .channel_close_function = sshClose,
+      .userdata = NULL,
+  };
 
   ssh_session sess = ssh_channel_get_session(chan);
-    socket_t fd;
-    struct termios *term = NULL;
-    struct winsize *win = NULL;
-    pid_t childpid;
-    ssh_event event;
-    short events;
+  ssh_callbacks_init(&cb);
+  ssh_set_channel_callbacks(chan, &cb);
 
-    cb.userdata = &fd;
-    ssh_callbacks_init(&cb);
-    ssh_set_channel_callbacks(chan, &cb);
+  short events = POLLIN | POLLPRI | POLLERR | POLLHUP | POLLNVAL;
 
-    events = POLLIN | POLLPRI | POLLERR | POLLHUP | POLLNVAL;
+  ssh_event event = ssh_event_new();
+  if (event == NULL) {
+    printf("Couldn't get a event\n");
+    return;
+  }
 
-    event = ssh_event_new();
-    if(event == NULL) {
-        printf("Couldn't get a event\n");
-        return -1;
-    }
+  if (ssh_event_add_session(event, sess) != SSH_OK) {
+    printf("Couldn't add the session to the event\n");
+    return;
+  }
 
-    if(ssh_event_add_fd(event, fd, events, copy_fd_to_chan, chan) != SSH_OK) {
-        printf("Couldn't add an fd to the event\n");
-        return -1;
-    }
-
-    if(ssh_event_add_session(event, sess) != SSH_OK) {
-        printf("Couldn't add the session to the event\n");
-        return -1;
-    }
-
-    do {
-        ssh_event_dopoll(event, 1000);
-    } while(!ssh_channel_is_closed(chan));
-
-    ssh_event_remove_fd(event, fd);
-    ssh_event_remove_session(event, sess);
-    ssh_event_free(event);
-
-
-
-
+  printf("user: %s\n", user);
 
   do {
-    i = ssh_channel_read(chan, buf, 2048, 0);
-    if (i > 0) {
-      // ssh_channel_write(chan, state, BOARD_BUFFER_SIZE);
-      // if (write(1, buf, i) < 0) {
-      //   printf("error writing to buffer\n");
-      //   return 1;
-      // }
-    }
-  } while (i > 0);
+    ssh_event_dopoll(event, 1000);
+  } while (!ssh_channel_is_closed(chan));
+
+  ssh_event_remove_session(event, sess);
+  ssh_event_free(event);
 
   ssh_disconnect(session);
-  ssh_bind_free(sshbind);
-  ssh_finalize();
+}
+
+void startSSH() {
+  ssh_bind sshbind = ssh_bind_new();
+  int port = SSHD_PORT;
+
+  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDPORT, &port);
+  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_HOSTKEY, "ssh-rsa");
+  ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_RSAKEY, KEYS_FILE);
+
+  if (ssh_bind_listen(sshbind) < 0) {
+    printf("Error listening to socket: %s\n", ssh_get_error(sshbind));
+    return;
+  }
+
+  while (1) {
+    pthread_t thread;
+    ssh_session session = ssh_new();
+    int r = ssh_bind_accept(sshbind, session);
+
+    if (r == SSH_ERROR) {
+      printf("error accepting a connection : %s\n", ssh_get_error(sshbind));
+      return;
+    }
+
+    pthread_create(&thread, NULL, handleConnection, session);
+  }
 }
--- src/ssh.h
+++ src/ssh.h
@@ -1,16 +1,16 @@
+#include <poll.h>
+#include <pthread.h>
+#include <pty.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <poll.h>
-#include <pty.h>
 
+#include <libssh/callbacks.h>
 #include <libssh/libssh.h>
 #include <libssh/server.h>
-#include <libssh/callbacks.h>
 
 #define LIBSSH_STATIC 1
-#define SSHD_USER "libssh"
-#define SSHD_PASSWORD "libssh"
 #define SSHD_PORT 4242
 #define KEYS_FILE "./keys/rsa"
 
